1768f06ad990f86e97bb6133bf4415978d2ffbe1
johan <johan@boot>
2020-10-01 19:13:56 +0200

    Configuration de la sécurité

--- webapp/src/main/java/com/boot/configuration/SecurityConfiguration.java
+++ webapp/src/main/java/com/boot/configuration/SecurityConfiguration.java
@@ -1,7 +1,14 @@
 package com.boot.configuration;
 
+import org.jasypt.springsecurity4.crypto.password.PasswordEncoder;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +19,13 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+  private AuthenticationProvider authenticationProvider;
+
+  @Autowired
+  @Qualifier("daoAuthenticationProvider")
+  public void setAuthenticationProvider(AuthenticationProvider authenticationProvider) {
+      this.authenticationProvider = authenticationProvider;
+  }
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
@@ -22,22 +36,39 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
 	}
+//	@Bean
+//	@Override
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user =
+//			 User.withDefaultPasswordEncoder()
+//				.username("user")
+//				.password("user")
+//				.roles("USER")
+//				.build();
+//		UserDetails admin =
+//				User.withDefaultPasswordEncoder()
+//				.username("admin")
+//				.password("admin")
+//				.roles("ADMIN")
+//				.build();
+//
+//		return new InMemoryUserDetailsManager(user,admin);
+//	}
 	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("user")
-				.roles("USER")
-				.build();
-		UserDetails admin =
-				User.withDefaultPasswordEncoder()
-				.username("admin")
-				.password("admin")
-				.roles("ADMIN")
-				.build();
-
-		return new InMemoryUserDetailsManager(user,admin);
+	public PasswordEncoder passwordEncoder(StrongPasswordEncryptor passwordEncryptor){
+	    PasswordEncoder passwordEncoder = new PasswordEncoder();
+	    passwordEncoder.setPasswordEncryptor(passwordEncryptor);
+	    return passwordEncoder;
+	}
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder, UserDetailsService userDetailsService){
+	    DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+	    daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+	    daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+	    return daoAuthenticationProvider;
+	}
+	@Autowired
+	public void configureAuthManager(AuthenticationManagerBuilder authenticationManagerBuilder){
+	    authenticationManagerBuilder.authenticationProvider(authenticationProvider);
 	}
 }
