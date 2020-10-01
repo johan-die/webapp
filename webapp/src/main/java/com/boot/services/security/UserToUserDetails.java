b6050428cf91a73fac3b3db9752ab557575c7328
johan <johan@boot>
2020-10-01 18:31:00 +0200

    Convertisseur user to userDetails

---
+++ webapp/src/main/java/com/boot/services/security/UserToUserDetails.java
@@ -0,0 +1,32 @@
+package com.boot.services.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import com.boot.entities.User;
+
+@Component
+public class UserToUserDetails implements Converter<User, UserDetails> {
+    @Override
+    public UserDetails convert(User user) {
+        UserDetailsImpl userDetails = new UserDetailsImpl();
+
+        if (user != null) {
+            userDetails.setUsername(user.getUsername());
+            userDetails.setPassword(user.getEncryptedPassword());
+            userDetails.setEnabled(user.getEnabled());
+            Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+            user.getRoles().forEach(role -> {
+                authorities.add(new SimpleGrantedAuthority(role.getRole()));
+            });
+            userDetails.setAuthorities(authorities);
+        }
+
+        return userDetails;
+    }
+}
